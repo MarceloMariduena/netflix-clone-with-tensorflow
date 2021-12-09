dee56ca23fca26e7db78890c09b649049880f372
toaster <toaster@localhost.localdomain>
2021-12-09 16:47:52 -0500

    Netflix logo and user icon added.

---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import "./Nav.css";
+import React, { useState, useEffect } from "react";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll", handleShow);
+    };
+  }, []);
+
+  return (
+    <div className={`nav && ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Netflix_logo.svg/800px-Netflix_logo.svg.png"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix Avatar Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
